91f30b56bd9fa900bb11936dfce0d8aec014ff8a
nathan <=>
2026-03-31 23:35:43 +1100

    feat: initialise event handler function for file ingestion

--- infrastructure/eventarc.tf
+++ infrastructure/eventarc.tf
@@ -3,10 +3,16 @@ resource "google_eventarc_trigger" "gcs-object-finalized" {
   location = var.region
   matching_criteria {
     attribute = "type"
-    value     = [
+    value = [
       "google.cloud.storage.object.v1.finalized",
       "google.cloud.storage.object.v1.metadataUpdated",
     ]
+  }
+  destination {
+    cloud_run_service {
+      service = google_cloud_run_service.default.name
+      region  = var.region
+    }
   }
   transport {
     pubsub {
